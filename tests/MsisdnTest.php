56421c13ed2544ecfa8b115e7c81f9bed5924316
Yan Santos <git@yansantos.com>
2020-05-10 04:17:27 +0800

    Updated MsisdnTest to match both value and type

--- tests/MsisdnTest.php
+++ tests/MsisdnTest.php
@@ -73,7 +73,7 @@ final class MsisdnTest extends TestCase
     public function testGetPrefix()
     {
         for ($i=0; $i<count($this->mobile); $i++) {
-            $this->assertEquals($this->prefix[$i], BashPH\Msisdn::getPrefix($this->mobile[$i])['prefix']);
+            $this->assertSame($this->prefix[$i], BashPH\Msisdn::getPrefix($this->mobile[$i])['prefix']);
         }
     }
 
@@ -83,7 +83,7 @@ final class MsisdnTest extends TestCase
     public function testErrorGetPrefix()
     {
         for ($i=0; $i<count($this->errorMobile); $i++) {
-            $this->assertEquals(false, BashPH\Msisdn::getPrefix($this->errorMobile[$i])['valid']);
+            $this->assertSame(false, BashPH\Msisdn::getPrefix($this->errorMobile[$i])['valid']);
         }
     }
 
@@ -93,22 +93,22 @@ final class MsisdnTest extends TestCase
     public function testFormat()
     {
         // with country code, no separator.
-        $this->assertEquals("+6391737654321", BashPH\Msisdn::format('091737654321', true));
+        $this->assertSame("+6391737654321", BashPH\Msisdn::format('091737654321', true));
 
         // with country code, separator is "-".
-        $this->assertEquals("+63-9175-765-4321", BashPH\Msisdn::format('+6391757654321', true, '-'));
+        $this->assertSame("+63-9175-765-4321", BashPH\Msisdn::format('+6391757654321', true, '-'));
 
         // with country code, separator is " ".
-        $this->assertEquals("+63 9176 765 4321", BashPH\Msisdn::format('091767654321', true, ' '));
+        $this->assertSame("+63 9176 765 4321", BashPH\Msisdn::format('091767654321', true, ' '));
 
         // with country code, separator is ".".
-        $this->assertEquals("+63.955.765.4321", BashPH\Msisdn::format('+639557654321', true, '.'));
+        $this->assertSame("+63.955.765.4321", BashPH\Msisdn::format('+639557654321', true, '.'));
 
         // no country code, separator is "-".
-        $this->assertEquals("0955-765-4321", BashPH\Msisdn::format('+639557654321', false, '-'));
+        $this->assertSame("0955-765-4321", BashPH\Msisdn::format('+639557654321', false, '-'));
 
         // no country code, no separator.
-        $this->assertEquals("09077654321", BashPH\Msisdn::format('9077654321'));
+        $this->assertSame("09077654321", BashPH\Msisdn::format('9077654321'));
     }
 
     /**
@@ -116,11 +116,11 @@ final class MsisdnTest extends TestCase
      */
     public function testSanitize()
     {
-        $this->assertEquals("6391737654321", BashPH\Msisdn::sanitize('+6391737654321'));
-        $this->assertEquals("6391737654321", BashPH\Msisdn::sanitize('+63-9173-765-4321'));
-        $this->assertEquals("6391737654321", BashPH\Msisdn::sanitize('+63 9173 765 4321'));
-        $this->assertEquals("6391737654321", BashPH\Msisdn::sanitize('+63 A9173 B765 C4321 X'));
-        $this->assertEquals("", BashPH\Msisdn::sanitize('+abcdefghi'));
+        $this->assertSame("6391737654321", BashPH\Msisdn::sanitize('+6391737654321'));
+        $this->assertSame("6391737654321", BashPH\Msisdn::sanitize('+63-9173-765-4321'));
+        $this->assertSame("6391737654321", BashPH\Msisdn::sanitize('+63 9173 765 4321'));
+        $this->assertSame("6391737654321", BashPH\Msisdn::sanitize('+63 A9173 B765 C4321 X'));
+        $this->assertSame("", BashPH\Msisdn::sanitize('+abcdefghi'));
     }
 
     /**
@@ -128,9 +128,9 @@ final class MsisdnTest extends TestCase
      */
     public function testRemoveLeadingZero()
     {
-        $this->assertEquals("91737654321", BashPH\Msisdn::removeLeadingZero('091737654321'));
-        $this->assertEquals("9177654321", BashPH\Msisdn::removeLeadingZero('09177654321'));
-        $this->assertEquals("", BashPH\Msisdn::removeLeadingZero(''));
+        $this->assertSame("91737654321", BashPH\Msisdn::removeLeadingZero('091737654321'));
+        $this->assertSame("9177654321", BashPH\Msisdn::removeLeadingZero('09177654321'));
+        $this->assertSame("", BashPH\Msisdn::removeLeadingZero(''));
     }
 
     /**
@@ -138,10 +138,10 @@ final class MsisdnTest extends TestCase
      */
     public function testRemoveCountryCode()
     {
-        $this->assertEquals("91737654321", BashPH\Msisdn::removeCountryCode('+6391737654321'));
-        $this->assertEquals("9177654321", BashPH\Msisdn::removeCountryCode('+639177654321'));
-        $this->assertEquals("9177654321", BashPH\Msisdn::removeCountryCode('639177654321'));
-        $this->assertEquals("", BashPH\Msisdn::removeCountryCode(''));
+        $this->assertSame("91737654321", BashPH\Msisdn::removeCountryCode('+6391737654321'));
+        $this->assertSame("9177654321", BashPH\Msisdn::removeCountryCode('+639177654321'));
+        $this->assertSame("9177654321", BashPH\Msisdn::removeCountryCode('639177654321'));
+        $this->assertSame("", BashPH\Msisdn::removeCountryCode(''));
     }
 
     /**
@@ -182,13 +182,13 @@ final class MsisdnTest extends TestCase
      */
     public function testClean()
     {
-        $this->assertEquals('91737654321', BashPH\Msisdn::clean('091737654321'));
-        $this->assertEquals('91737654321', BashPH\Msisdn::clean('+6391737654321'));
-        $this->assertEquals('91737654321', BashPH\Msisdn::clean('09173-765-4321'));
-        $this->assertEquals('91737654321', BashPH\Msisdn::clean('+63-9173-765-4321'));
-        $this->assertEquals('91737654321', BashPH\Msisdn::clean('+63 9173 765 4321'));
-        $this->assertEquals('91737654321', BashPH\Msisdn::clean('+63 A9173 B765 C4321'));
-        $this->assertEquals('', BashPH\Msisdn::clean('abcdefghijklmnop'));
+        $this->assertSame('91737654321', BashPH\Msisdn::clean('091737654321'));
+        $this->assertSame('91737654321', BashPH\Msisdn::clean('+6391737654321'));
+        $this->assertSame('91737654321', BashPH\Msisdn::clean('09173-765-4321'));
+        $this->assertSame('91737654321', BashPH\Msisdn::clean('+63-9173-765-4321'));
+        $this->assertSame('91737654321', BashPH\Msisdn::clean('+63 9173 765 4321'));
+        $this->assertSame('91737654321', BashPH\Msisdn::clean('+63 A9173 B765 C4321'));
+        $this->assertSame('', BashPH\Msisdn::clean('abcdefghijklmnop'));
     }
 
     /**
@@ -219,17 +219,17 @@ final class MsisdnTest extends TestCase
         //091737654321
 
         //prefix
-        $this->assertEquals("9173", $validation[0]['prefix']);
+        $this->assertSame("9173", $validation[0]['prefix']);
 
         //network
-        $this->assertEquals("Globe", $validation[0]['carrier']["network"]);
-        $this->assertEquals("Postpaid", $validation[0]['carrier']["type"]);
+        $this->assertSame("Globe", $validation[0]['carrier']["network"]);
+        $this->assertSame("Postpaid", $validation[0]['carrier']["type"]);
 
         //format with country code
-        $this->assertEquals("+6391737654321", $validation[0]['format'][0]);
+        $this->assertSame("+6391737654321", $validation[0]['format'][0]);
 
         //format with leading zero
-        $this->assertEquals("091737654321", $validation[0]['format'][1]);
+        $this->assertSame("091737654321", $validation[0]['format'][1]);
 
         //verify no error
         $this->assertNull($validation[0]['error']);
@@ -238,19 +238,19 @@ final class MsisdnTest extends TestCase
         $this->assertTrue(BashPH\Msisdn::validate($this->mobile[0], 'Globe')['valid']);
 
         //validate by network, get format with country code
-        $this->assertEquals("+6391737654321", BashPH\Msisdn::validate($this->mobile[0], 'Globe')['format'][0]);
+        $this->assertSame("+6391737654321", BashPH\Msisdn::validate($this->mobile[0], 'Globe')['format'][0]);
 
         //validate by network, get format with leading zero
-        $this->assertEquals("091737654321", BashPH\Msisdn::validate($this->mobile[0], 'Globe')['format'][1]);
+        $this->assertSame("091737654321", BashPH\Msisdn::validate($this->mobile[0], 'Globe')['format'][1]);
 
         //validate by network with separator
         $this->assertTrue(BashPH\Msisdn::validate($this->mobile[0], 'Globe', '-')['valid']);
 
         //validate by network with separator, get format with country code
-        $this->assertEquals("+63-9173-765-4321", BashPH\Msisdn::validate($this->mobile[0], 'Globe', '-')['format'][0]);
+        $this->assertSame("+63-9173-765-4321", BashPH\Msisdn::validate($this->mobile[0], 'Globe', '-')['format'][0]);
 
         //validate by network with separator, get format with leading zero
-        $this->assertEquals("09173-765-4321", BashPH\Msisdn::validate($this->mobile[0], 'Globe', '-')['format'][1]);
+        $this->assertSame("09173-765-4321", BashPH\Msisdn::validate($this->mobile[0], 'Globe', '-')['format'][1]);
     }
 
     /**
